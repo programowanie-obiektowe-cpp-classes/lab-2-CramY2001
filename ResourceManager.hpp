698298eee1d927057e3fa55f246c6b9bf8f05caf
CramY2001 <szymek.kramarczyk@gmail.com>
2024-10-30 23:00:11 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,36 @@
 #pragma once
-
 #include "Resource.hpp"
-
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+  ResourceManager()
+  {
+    resource_ptr = new Resource();
+  }
+  ~ResourceManager()
+  {
+    delete resource_ptr;
+  }
+  ResourceManager(const ResourceManager& other)
+  {
+    resource_ptr = new Resource(*other.resource_ptr);
+  }
+  ResourceManager& operator=(const ResourceManager& other)
+  {
+    if (this != &other) {
+      delete resource_ptr;
+      resource_ptr = new Resource(*other.resource_ptr);
+    }
+    return *this;
+  }
+  double getResourceValue() const
+  {
+    return (*resource_ptr).tab[Resource::get_index];
+  }
+  double get()
+  {
+    return (*resource_ptr).get();
+  }
+private:
+  Resource* resource_ptr;
 };
